5bf77cc39f3ce9d9be06ebcc12f6d587c4ae1847
bnewbold <bnewbold@robocracy.org>
2016-04-25 23:53:12 -0400

    update (and line) prelude.scm

--- prelude.scm
+++ prelude.scm
@@ -15,10 +15,18 @@
 (define cddar (lambda (x) (car (cdr (cdr x)))))
 (define cdddr (lambda (x) (car (cdr (cdr x)))))
 
+; my favorite!
+(define cdaddr (lambda (x) (cdr (car (cdr (cdr x))))))
+
 (define if (lambda (pred tval fval) (cond (pred tval) (else fval))))
 (define not (lambda (x) (if x #f #t)))
 
 (define abs (lambda (x) (if (> x 0) x (* -1 x))))
 
-;(define for-each map)
+(define map
+  (lambda (f l)
+    (cond ((null? l) ())
+          (else (cons (f (car l)) (map f (cdr l)))))))
+
+(define for-each map)
 ;(define compose (lambda (f g) (lambda args (f (apply g args)))))
